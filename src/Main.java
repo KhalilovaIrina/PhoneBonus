5b295f13c58996e01f7eb2c96042142f5b7f43a2
Khalilova <sharova-ira90@yandex.ru>
2022-12-14 13:50:34 +0300

    Correct code

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,16 @@ public class Main {
     public static void main(String[] args) {
 
         int initialAmount = 100;
-        int depositAmount = 3000;
+        int depositAmount = 1100;
 
 
         if (depositAmount > 1000) {
             System.out.println("На вашем счете " + (initialAmount + depositAmount + (depositAmount / 100)) + " рублей.");
+            System.out.println("Бонус: " + (depositAmount / 100) + " рублей.");
+
         } else {
             System.out.println("На вашем счете " + (initialAmount + depositAmount) + " рублей.");
+            System.out.println("Бонус: 0 рублей.");
         }
 
     }
